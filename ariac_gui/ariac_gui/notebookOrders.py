ef9c769be45aa15cfab4241bdd3f3c212bd4e049
Boogsy679 <jothomas0615@gmail.com>
2023-02-28 17:01:03 +0000

    removed order category from order menu

--- ariac_gui/ariac_gui/notebookOrders.py
+++ ariac_gui/ariac_gui/notebookOrders.py
@@ -18,7 +18,7 @@ allProdTypes=["sensor", "pump", "regulator", "battery"]
 allProdColors=['green', 'red', 'purple','blue','orange']
 conditionTypes=['time','partPlace','submission']
 def showNewOrderMenu(orderWidgetsArr, orderValsArr, usedIDs):
-    orderValsArr[0].set(orderCategories[0])
+    #orderValsArr[0].set(orderCategories[0])
     orderValsArr[1].set(orderTypes[0])
     orderValsArr[2].set('0')
     orderValsArr[3].set(conditionTypes[0])
@@ -32,8 +32,8 @@ def showNewOrderMenu(orderWidgetsArr, orderValsArr, usedIDs):
     orderValsArr[11].set(kittingDestinations[0])
     orderValsArr[12].set("")
     orderValsArr[13].set(generateOrderId(usedIDs))
-    orderWidgetsArr[0].grid(column=2, row=0)
-    orderWidgetsArr[1].grid(column=2, row=1)
+    '''orderWidgetsArr[0].grid(column=2, row=0)
+    orderWidgetsArr[1].grid(column=2, row=1)'''
     orderWidgetsArr[2].grid(column=2, row=2)
     orderWidgetsArr[3].grid(column=2, row=3)
     orderWidgetsArr[4].grid(column=2, row=4)

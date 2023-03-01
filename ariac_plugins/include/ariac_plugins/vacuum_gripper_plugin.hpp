9c7ce28bb4764c19366124b7242934cc8e3b7945
zeidk <zeidk@umd.edu>
2023-03-01 15:48:29 -0500

    tested dropped part challenge

--- ariac_plugins/include/ariac_plugins/vacuum_gripper_plugin.hpp
+++ ariac_plugins/include/ariac_plugins/vacuum_gripper_plugin.hpp
@@ -8,33 +8,33 @@
 
 namespace ariac_plugins
 {
-// Forward declaration of private data class.
-class VacuumGripperPluginPrivate;
+  // Forward declaration of private data class.
+  class VacuumGripperPluginPrivate;
 
-/// Example ROS-powered Gazebo plugin with some useful boilerplate.
-/// \details This is a `ModelPlugin`, but it could be any supported Gazebo plugin type, such as
-/// System, Visual, GUI, World, Sensor, etc.
-class VacuumGripperPlugin : public gazebo::ModelPlugin
-{
-public:
-  /// Constructor
-  VacuumGripperPlugin();
+  /// Example ROS-powered Gazebo plugin with some useful boilerplate.
+  /// \details This is a `ModelPlugin`, but it could be any supported Gazebo plugin type, such as
+  /// System, Visual, GUI, World, Sensor, etc.
+  class VacuumGripperPlugin : public gazebo::ModelPlugin
+  {
+  public:
+    /// Constructor
+    VacuumGripperPlugin();
 
-  /// Destructor
-  virtual ~VacuumGripperPlugin();
+    /// Destructor
+    virtual ~VacuumGripperPlugin();
 
-  /// Gazebo calls this when the plugin is loaded.
-  /// \param[in] model Pointer to parent model. Other plugin types will expose different entities,
-  /// such as `gazebo::sensors::SensorPtr`, `gazebo::physics::WorldPtr`,
-  /// `gazebo::rendering::VisualPtr`, etc.
-  /// \param[in] sdf SDF element containing user-defined parameters.
-  void Load(gazebo::physics::ModelPtr model, sdf::ElementPtr sdf) override;
+    /// Gazebo calls this when the plugin is loaded.
+    /// \param[in] model Pointer to parent model. Other plugin types will expose different entities,
+    /// such as `gazebo::sensors::SensorPtr`, `gazebo::physics::WorldPtr`,
+    /// `gazebo::rendering::VisualPtr`, etc.
+    /// \param[in] sdf SDF element containing user-defined parameters.
+    void Load(gazebo::physics::ModelPtr model, sdf::ElementPtr sdf) override;
 
-private:
-  /// Recommended PIMPL pattern. This variable should hold all private
-  /// data members.
-  std::unique_ptr<VacuumGripperPluginPrivate> impl_;
-};
-}  // namespace gazebo_plugins
+  private:
+    /// Recommended PIMPL pattern. This variable should hold all private
+    /// data members.
+    std::unique_ptr<VacuumGripperPluginPrivate> impl_;
+  };
+} // namespace gazebo_plugins
 
-#endif  // VACUUM_GRIPPER_PLUGIN_HPP_
+#endif // VACUUM_GRIPPER_PLUGIN_HPP_

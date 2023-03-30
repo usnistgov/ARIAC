9bb26632da8f0f56ae825f227b282a11567dd2b4
zeidk <zeidk@umd.edu>
2023-03-29 23:42:38 -0400

    updated the human challenge description

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ extensions = [
     # External stuff
     "myst_parser",
     "sphinx_copybutton",
-    'sphinx.ext.inlinesyntaxhighlight'
+    'sphinxcontrib.inlinesyntaxhighlight'
 ]
 
 todo_include_todos = True

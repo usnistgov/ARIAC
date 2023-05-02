297c5200e063d4f985af5570ee5a628af8e9b03b
Joe Fernandez <52220317+jfernandez37@users.noreply.github.com>
2023-05-02 12:16:46 -0400

    Updated with new file paths

--- automated_evaluation/runDockerTest.sh
+++ automated_evaluation/runDockerTest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -d --name ariac_container -p 6080:80 --shm-size=512m -v $PWD/autoEval:/home/ubuntu/autoEval ariac:ariac_latest
+docker run -d --name ariac_container -p 6080:80 --shm-size=512m -v $PWD:/home/ubuntu/autoEval ariac:ariac_latest
 sleep 10
 docker cp ./runTest.sh ariac_container:/home/ubuntu
-docker exec -it ariac_container bash -c ". /home/ubuntu/ariac_ws/src/ariac/automated_evaluation/runTest.sh"
+docker exec -it ariac_container bash -c ". /home/ubuntu/runTest.sh"

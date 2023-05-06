7d0e25c5bb2294d119d2023b1af5ccb9a6408952
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2023-05-06 05:03:29 -0400

    autoeval: Fix inside Docker container 1) wrong path, 2) Outdated local git repo

--- automated_evaluation/runTest.sh
+++ automated_evaluation/runTest.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-cd ../home/ubuntu/ariac_ws/src/ariac/automated_evaluation/autoEval
+export ARIAC_REPO_HOME=/home/ubuntu/ariac_ws/src/ariac/
+cd $ARIAC_REPO_HOME
+echo "BEGIN; Temp as of 2023/05/05. The local repo '${ARIAC_REPO_HOME}' in the provided Docker image is outdated, so synching with the remote. Also switching to the temp dev branch '${TEMP_DEV_BRANCH}'."
+export TEMP_DEV_BRANCH=documentation_staging
+git fetch origin && git checkout documentation_staging && git rebase origin/${TEMP_DEV_BRANCH}
+echo "END: Temp as of 2023/05/05"
+cd $ARIAC_REPO_HOME/automated_evaluation/autoEval
 chmod +x runAll.sh
 chmod +x runLaunch.sh
 chmod +x read_competitor_config.py

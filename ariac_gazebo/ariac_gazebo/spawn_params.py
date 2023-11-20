7c4ea508634ab7c146dc7d0aa2ab548819d51106
Justin Albrecht <50928580+jaybrecht@users.noreply.github.com>
2023-11-20 07:55:40 -0500

    package cleanup (#261)

--- ariac_gazebo/ariac_gazebo/spawn_params.py
+++ ariac_gazebo/ariac_gazebo/spawn_params.py
@@ -67,12 +67,6 @@ class SensorSpawnParams(SpawnParams):
             plugin.find('camera_name').text = self.name
             plugin.find('frame_name').text = self.name + "_frame"
 
-        # scoring_sensors = ['agv_tray_sensor', 'assembly_station_sensor']
-        # if self.sensor_type in scoring_sensors:
-        #     plugin = xml.find('model').find('link').find('sensor').find('plugin')
-        #     plugin.set('name', str(self.name + "_ros_plugin"))
-        #     plugin.find('sensor_num').text = self.name[-1]
-
         self.xml = ET.tostring(xml, encoding="unicode")
 
 

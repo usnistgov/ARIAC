ee15e97234bbf855a6dda16efe8637729a0d7f8a
Justin <jalbrecht23@gmail.com>
2023-12-11 14:30:41 -0500

    update moveit_test launch file to match competitor.launch

--- test_competitor/launch/moveit_test.launch.py
+++ test_competitor/launch/moveit_test.launch.py
@@ -1,87 +1,81 @@
 import os
-import yaml
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.actions import (
+    DeclareLaunchArgument,
+    OpaqueFunction,
+)
+
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch.conditions import IfCondition
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
+
 from launch_ros.substitutions import FindPackageShare
 
 from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
 
 
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
+def launch_setup(context, *args, **kwargs):
+    urdf = os.path.join(get_package_share_directory("ariac_description"), "urdf/ariac_robots/ariac_robots.urdf.xacro")
 
-    try:
-        with open(absolute_file_path, "r") as file:
-            return file.read()
-    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
-        return None
-
-def load_yaml(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, "r") as file:
-            return yaml.safe_load(file)
-    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
-        return None
-
-
-def generate_launch_description():
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution([FindPackageShare("ariac_description"), "urdf/ariac_robots", "ariac_robots.urdf.xacro"]), 
-            " "
-        ]
+    moveit_config = (
+        MoveItConfigsBuilder("ariac_robots", package_name="ariac_moveit_config")
+        .robot_description(urdf)
+        .robot_description_semantic(file_path="config/ariac_robots.srdf")
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .planning_pipelines(pipelines=["ompl"])
+        .to_moveit_configs()
     )
-
-    robot_description = {"robot_description": robot_description_content}
-    
-    ## Moveit Parameters
-    robot_description_semantic = {"robot_description_semantic": load_file("ariac_moveit_config", "srdf/ariac_robots.srdf")}
-
-    robot_description_kinematics = {"robot_description_kinematics": load_yaml("ariac_moveit_config", "config/kinematics.yaml")}
 
     moveit_test = Node(
         package="test_competitor",
         executable="moveit_test",
         output="screen",
         parameters=[
-            robot_description,
-            robot_description_semantic,
-            robot_description_kinematics,
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            moveit_config.joint_limits,
             {"use_sim_time": True},
         ],
+        arguments=['--ros-args', '--log-level', 'move_group_interface:=warn', '--log-level', 'moveit_trajectory_processing.time_optimal_trajectory_generation:=error']
     )
 
-    # RVIZ 
+    start_rviz = LaunchConfiguration("rviz")
+
     rviz_config_file = PathJoinSubstitution(
         [FindPackageShare("test_competitor"), "rviz", "test_competitor.rviz"]
     )
 
-    rviz = Node(
+    rviz_node = Node(
         package="rviz2",
         executable="rviz2",
+        name="rviz2_moveit",
+        output="log",
+        arguments=["-d", rviz_config_file],
         parameters=[
-            robot_description,
-            robot_description_semantic,
-            robot_description_kinematics,
-            {"use_sim_time": True},
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            moveit_config.joint_limits,
+            {"use_sim_time": True}
         ],
-        arguments=['-d', rviz_config_file]
+        condition=IfCondition(start_rviz)
     )
 
     nodes_to_start = [
-        moveit_test, 
-        rviz
+        moveit_test,
+        rviz_node
     ]
 
-    return LaunchDescription(nodes_to_start)
+    return nodes_to_start
+
+def generate_launch_description():
+    declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument("rviz", default_value="false", description="start rviz node?")
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])

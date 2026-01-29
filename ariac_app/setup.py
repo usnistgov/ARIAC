4ed43a401da5131c4efe23a33e539e09b40ff40d
jfernandez37 <joseph.fernandez@nist.gov>
2026-01-29 09:55:35 -0500

    Updated changelogs for 2025.3.1

--- ariac_app/setup.py
+++ ariac_app/setup.py
@@ -4,7 +4,7 @@ package_name = 'ariac_app'
 
 setup(
     name=package_name,
-    version='2025.3.0',
+    version='2025.3.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
--- ariac_db/setup.py
+++ ariac_db/setup.py
@@ -6,7 +6,7 @@ package_name = 'ariac_db'
 
 setup(
     name=package_name,
-    version='2025.3.0',
+    version='2025.3.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',

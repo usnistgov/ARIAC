314001eb44c4ef31a3b96ed9b4472431d7c2d66c
jfernandez37 <joseph.fernandez@nist.gov>
2026-01-29 09:20:05 -0500

    AGVs give penalty and remove object when in contact

--- ariac_plugins/include/ariac_plugins/agv_tray_interface_plugin.hpp
+++ ariac_plugins/include/ariac_plugins/agv_tray_interface_plugin.hpp
@@ -39,6 +39,7 @@
 #include "angles/angles.h"
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <tinyxml2.h>
+#include <mutex>
 
 using AGVStations = ariac_interfaces::msg::AgvStations;
 using AGVStatus = ariac_interfaces::msg::AgvStatus;
@@ -80,6 +81,7 @@ namespace ariac_plugins
       
       int agv_station = AGVStations::INSPECTION;
       int tray_index = 0;
+      int insert_iteration = -1;
       
       TrayState tray_status = TrayState::INSERTING;
 
@@ -104,6 +106,7 @@ namespace ariac_plugins
       gz::sim::Entity agv_base_link_entity;
 
       std::optional<ariac_components::Kit> kit_component = std::nullopt; 
+      std::mutex kit_mutex_;
 
       gz::math::Pose3d tray_transform = gz::math::Pose3d(0.0, 0.0, 0.35, 0.0, 0.0, 1.57);
       gz::math::Pose3d tray_spawn_transform = gz::math::Pose3d(0.0, 0.0, 0.3501, 0.0, 0.0, 1.57);
